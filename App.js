e300bdc8f20d107162a78de12a70f29e906b45e4
Ganasa18 <ganasa18@yahoo.com>
2022-04-22 15:23:38 +0700

    updated course 1

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from "react-native";
+import React from "react";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { HomeScreen } from "./src/pages";
+import RootNavigator from "./src/router/RootNavigator";
+
+const App = () => {
+  return <RootNavigator />;
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
